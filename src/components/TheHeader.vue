b7036adc25bd8ea349f34076664c03efa52f1143
TripleNine666 <kolumb1709@gmail.com>
2023-03-21 18:18:45 +0300

    header icon

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="google-header">
+  <div class="google-header header-height">
     <div class="actions">
       <div class="links">
         <div class="link__item">
@@ -9,9 +9,15 @@
           <a href="https://www.google.nl/imghp?hl=ru&amp;ogbl">Картинки</a>
         </div>
       </div>
-      <div>
-        <div>Иконка1</div>
-        <div>Иконка2</div>
+      <div class="icons">
+        <div class="icon__item">
+          <a
+            class="material-symbols-outlined"
+            href="https://www.google.nl/imghp?hl=ru&amp;ogbl"
+            ><i class="material-symbols-outlined">Apps</i></a
+          >
+        </div>
+        <div class="icon__item"><a href="#">Иконка2</a></div>
       </div>
     </div>
   </div>
@@ -23,12 +29,13 @@ export default {};
 
 <style scoped>
 .google-header {
-  height: 60px;
   display: flex;
-  border: 2px solid white;
-  padding: 10px;
   justify-content: flex-end;
   align-items: center;
+}
+.header-height {
+  height: 60px;
+  padding: 6px;
 }
 .actions {
   display: flex;
@@ -46,15 +53,40 @@ export default {};
 .link__item {
   display: inline-block;
 }
-.links a {
+.link__item a {
   display: inline-block;
-  padding: 10px;
-  font-family: arial, sans-serif;
-  font-stretch: 100%;
+  padding: 8px;
+  font: 13px/27px Roboto, Arial, sans-serif;
   text-decoration: none;
   color: white;
 }
+.icon__item a {
+  display: inline-block;
+  padding: 8px;
+  text-decoration: none;
+  color: white;
+}
+i {
+  display: inline-block;
+}
+.icons {
+  display: flex;
+  height: 48px;
+  align-items: center;
+  vertical-align: middle;
+}
 .links a:hover {
   text-decoration: underline;
+}
+.icon__item {
+  display: inline-block;
+}
+.icon__item a:hover {
+  background-color: rgba(136, 140, 143, 0.363);
+  border-radius: 50%;
+}
+
+.material-symbols-outlined {
+  font-variation-settings: "FILL" 0, "wght" 600, "GRAD" 25, "opsz" 48;
 }
 </style>
