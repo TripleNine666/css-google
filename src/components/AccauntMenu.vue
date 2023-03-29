e6c8ce98df3ff581c66e44fe28dd779dc9513d1e
TripleNine666 <kolumb1709@gmail.com>
2023-03-29 09:14:19 +0300

    AccauntMenu Component

---
+++ src/components/AccauntMenu.vue
@@ -0,0 +1,385 @@
+<template>
+  <div class="accaunt-window-container">
+    <div class="accaunt-window-container__main">
+      <div>
+        <div class="accaunt-window__items">
+          <div class="account-manage-item">
+            <div class="account-manage-item-set">
+              <div class="manage-item__block">
+                <div class="accaunt-control">
+                  <div class="accaunt-control__name">
+                    <div class="accaunt-control__name__photo">
+                      <img
+                        jsname="wsYqfb"
+                        alt="Изображение профиля"
+                        class="JZFjpd"
+                        src="https://lh3.googleusercontent.com/a/AGNmyxZzBqUOiFWwjAgIbrjiPZAFyjm_wgFTtfjTMZ3h=s83-c-mo"
+                        srcset="
+                          https://lh3.googleusercontent.com/a/AGNmyxZzBqUOiFWwjAgIbrjiPZAFyjm_wgFTtfjTMZ3h=s83-c-mo  1x,
+                          https://lh3.googleusercontent.com/a/AGNmyxZzBqUOiFWwjAgIbrjiPZAFyjm_wgFTtfjTMZ3h=s192-c-mo 2x
+                        "
+                      />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="64"
+                        height="64"
+                        viewBox="0 0 64 64"
+                        fill="none"
+                      >
+                        <mask id="path-1-inside-1_824_12075" fill="white">
+                          <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M62.5796 41.4569C63.5029 38.468 64 35.2921 64 32C64 14.3269 49.6731 0 32 0C14.3269 0 0 14.3269 0 32C0 49.6731 14.3269 64 32 64C35.2921 64 38.468 63.5029 41.4569 62.5796C39.2981 59.9812 38 56.6422 38 53C38 44.7157 44.7157 38 53 38C56.6422 38 59.9812 39.2981 62.5796 41.4569Z"
+                          ></path>
+                        </mask>
+                        <path
+                          d="M62.5796 41.4569L61.9406 42.2261L63.0929 43.1835L63.5351 41.7521L62.5796 41.4569ZM41.4569 62.5796L41.7521 63.5351L43.1835 63.0929L42.2261 61.9406L41.4569 62.5796ZM63 32C63 35.1909 62.5182 38.2674 61.6241 41.1618L63.5351 41.7521C64.4875 38.6687 65 35.3933 65 32H63ZM32 1C49.1208 1 63 14.8792 63 32H65C65 13.7746 50.2254 -1 32 -1V1ZM1 32C1 14.8792 14.8792 1 32 1V-1C13.7746 -1 -1 13.7746 -1 32H1ZM32 63C14.8792 63 1 49.1208 1 32H-1C-1 50.2254 13.7746 65 32 65V63ZM41.1618 61.6241C38.2674 62.5182 35.1909 63 32 63V65C35.3933 65 38.6687 64.4875 41.7521 63.5351L41.1618 61.6241ZM42.2261 61.9406C40.2109 59.515 39 56.4003 39 53H37C37 56.8841 38.3853 60.4474 40.6877 63.2186L42.2261 61.9406ZM39 53C39 45.268 45.268 39 53 39V37C44.1634 37 37 44.1634 37 53H39ZM53 39C56.4003 39 59.515 40.2109 61.9406 42.2261L63.2186 40.6877C60.4474 38.3853 56.8841 37 53 37V39Z"
+                          fill="currentColor"
+                          mask="url(#path-1-inside-1_824_12075)"
+                        ></path>
+                      </svg>
+                      <div class="camera-icon">
+                        <button class="camera-icon">
+                          <svg
+                            width="18"
+                            height="18"
+                            viewBox="0 0 24 24"
+                            focusable="false"
+                            class="camera-svg"
+                          >
+                            <path
+                              d="M20 5h-3.17L15 3H9L7.17 5H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 14H4V7h16v12z"
+                            ></path>
+                            <path
+                              d="M12 9c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"
+                            ></path>
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                    <div class="accaunt-control__name__info">
+                      <div class="accaunt-control__name__info__fio">
+                        Кирилл Колубович
+                      </div>
+                      <div class="accaunt-control__name__info__gmail">
+                        kolumb1709@gmail.com
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accaunt-control__button">
+                    <a
+                      href="https://myaccount.google.com/?hl=ru&utm_source=OGB&utm_medium=act"
+                      >Управление аккаунтом Google</a
+                    >
+                  </div>
+                </div>
+                <div class="accaunt-control-line"></div>
+              </div>
+              <div class="manage-item__block add-accaunt">
+                <a
+                  class="add-accaunt__a"
+                  href="https://accounts.google.com/AddSession?hl=ru&continue=https://www.google.com/&ec=GAlAmgQ&hl=ru&authuser=0"
+                >
+                  <span class="window-img">
+                    <div class="window-img__block">
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        focusable="false"
+                      >
+                        <path
+                          d="M9 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0-6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2zm0 7c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4zm6 5H3v-.99C3.2 16.29 6.3 15 9 15s5.8 1.29 6 2v1zm3-4v-3h-3V9h3V6h2v3h3v2h-3v3h-2z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </span>
+                  <span class="message-block">
+                    <div class="window-message">Добавить аккаунт</div>
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="account-exit-item">
+            <a
+              href="https://accounts.google.com/Logout?hl=ru&continue=https://www.google.com/&timeStmp=1680003440&secTok=.AG5fkS9Tur9nsD3_9GQKtW6Bwm31zPqMSA&ec=GAdAmgQ&hl=ru"
+              class="add-accaunt__a"
+            >
+              <span class="window-img">
+                <div class="window-img__block">
+                  <svg
+                    height="24"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    focusable="false"
+                  >
+                    <path
+                      d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"
+                    ></path>
+                    <path d="M0 0h24v24H0z" fill="none"></path>
+                  </svg>
+                </div>
+              </span>
+              <span class="message-block">
+                <div class="window-message">Выйти</div>
+              </span>
+            </a>
+          </div>
+          <div class="account-additional-item">
+            <a href="https://policies.google.com/privacy?hl=ru&authuser=0"
+              >Политика конфиденциальности</a
+            >
+            <span>&nbsp;•&nbsp;</span>
+            <a href="https://policies.google.com/terms?hl=ru&authuser=0"
+              >Условия использования</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.accaunt-window-container {
+  height: 100%;
+  -webkit-font-smoothing: antialiased;
+  color: rgba(0, 0, 0, 0.87);
+  font-family: Roboto, RobotoDraft, Helvetica, Arial, sans-serif;
+  margin: 0;
+  text-size-adjust: 100%;
+}
+.accaunt-window-container__main {
+  max-width: 400px;
+  padding: 8px 12px 16px;
+  height: 100%;
+}
+.accaunt-window-container__main > div {
+  background: #2d2e30;
+  overflow-y: auto;
+  border: none;
+  border-radius: 28px;
+  box-shadow: 0 4px 8px 3px rgba(0, 0, 0, 0.15), 0 1px 3px rgba(0, 0, 0, 0.3);
+  overflow-x: auto;
+  height: 100%;
+}
+.accaunt-window__items {
+  overflow-y: overlay;
+  height: 100%;
+  min-width: 240px;
+  padding-top: 4px;
+}
+.account-manage-item {
+  border-radius: 24px;
+  margin: 4px 8px 0;
+}
+.account-manage-item-set {
+  border-radius: inherit;
+  overflow: hidden;
+  padding: 0;
+  position: relative;
+  text-align: left;
+  white-space: nowrap;
+}
+.manage-item__block {
+  background: #1f1f1f;
+  border-radius: 4px;
+  margin-bottom: 2px;
+}
+.manage-item__block:first-child {
+  border-top-left-radius: inherit;
+  border-top-right-radius: inherit;
+}
+.manage-item__block:last-child {
+  border-bottom-left-radius: inherit;
+  border-bottom-right-radius: inherit;
+}
+.accaunt-control {
+  display: inline-block;
+  padding: 16px 16px 0;
+  vertical-align: top;
+  width: 100%;
+}
+.accaunt-control-line {
+  box-sizing: content-box;
+  left: 0;
+  min-height: 15px;
+  width: auto;
+}
+.accaunt-control__name {
+  display: inline-block;
+  margin-bottom: 1px;
+  position: relative;
+  vertical-align: top;
+  width: 100%;
+}
+.accaunt-control__name__photo {
+  display: inline-block;
+  position: relative;
+  text-align: center;
+  height: 64px;
+  width: 64px;
+  margin: 0;
+}
+.accaunt-control__name__photo img {
+  height: 64px;
+  width: 64px;
+  border-radius: 50%;
+}
+.accaunt-control__name__photo > svg {
+  color: rgba(241, 243, 244, 0.12);
+  left: 0;
+  top: 0;
+  border-radius: 50%;
+  position: absolute;
+}
+.camera-icon {
+  border-radius: 50%;
+  bottom: -4px;
+  position: absolute;
+  right: -4px;
+  background: #1f1f1f;
+}
+.camera-icon button {
+  margin: 3px;
+  border: 1px solid #444746;
+  position: relative;
+  bottom: 0;
+  right: 0;
+  height: 24px;
+  width: 24px;
+  padding: 0;
+  cursor: pointer;
+  border-radius: 50%;
+}
+.camera-icon button:hover {
+  background: rgba(227, 227, 227, 0.08);
+}
+.camera-svg {
+  color: #c4c7c5;
+  fill: currentColor;
+}
+.accaunt-control__name__info {
+  display: inline-block;
+  margin-bottom: 0;
+  margin-left: 14px;
+  margin-top: 13px;
+  max-width: calc(100% - 78px);
+  overflow: hidden;
+  text-overflow: ellipsis;
+  vertical-align: top;
+}
+.accaunt-control__name__info__fio {
+  color: rgb(227, 227, 227);
+  font-size: 14px;
+  font-weight: 600;
+  margin-bottom: 2px;
+}
+.accaunt-control__name__info__gmail {
+  color: rgb(196, 199, 197);
+  font-size: 12px;
+}
+.accaunt-control__button {
+  width: 100%;
+  overflow: hidden;
+}
+.accaunt-control__button a {
+  text-decoration: none;
+  display: inline-block;
+  color: rgb(227, 227, 227);
+  padding: 5px 15px;
+  border: 1px solid #747775;
+  margin-bottom: 9px;
+  margin-left: 78px;
+  margin-top: 8px;
+  border-radius: 8px;
+  font-size: 15px;
+}
+.accaunt-control__button a:hover {
+  background: rgba(227, 227, 227, 0.08);
+}
+.add-accaunt {
+  border: 0;
+}
+.add-accaunt__a {
+  display: block;
+  text-decoration: none;
+  color: #e8eaed;
+  padding: 0 15px 0 31px;
+  border: 1px solid transparent;
+  height: 48px;
+  width: 100%;
+  position: relative;
+}
+.add-accaunt__a:hover {
+  background: #2f3339;
+}
+.window-img {
+  display: inline-block;
+  padding: 11px 4px;
+  width: 32px;
+}
+.window-img__block {
+  height: 24px;
+  width: 24px;
+  padding: 0;
+  vertical-align: middle;
+}
+.window-img__block svg {
+  fill: currentColor;
+}
+.message-block {
+  display: inline-block;
+  margin-left: 30px;
+  max-width: 250px;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 24px;
+}
+.window-message {
+  max-height: 40px;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  display: -webkit-box;
+  overflow: hidden;
+}
+.account-exit-item {
+  border-bottom: 1px solid #7477753d;
+}
+.account-exit-item a {
+  padding: 0 23px 0 39px;
+}
+.account-additional-item {
+  height: 50px;
+  padding: 17px 0;
+  text-align: center;
+}
+.account-additional-item span {
+  color: #e3e3e3;
+}
+.account-additional-item span:hover {
+  background-color: rgba(232, 234, 237, 0.04);
+}
+.account-additional-item a {
+  text-decoration: none;
+  color: #e3e3e3;
+  font-size: 12px;
+  font-weight: 500;
+  letter-spacing: 0.1px;
+  line-height: 14px;
+  vertical-align: text-top;
+  padding: 4px 7px;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  text-align: right;
+}
+.account-additional-item a:hover {
+  background: #373b41;
+}
+</style>
